c41db4e03f3cea793d3852527c7e4a1bf49fda2a
Shen Yi Chen <kuys5220@gmail.com>
2022-07-31 16:49:12 +0800

    Linked library and use in main.

---
+++ lib/math/operations.hpp
@@ -0,0 +1,9 @@
+namespace math {
+    class operations{
+    public:
+        int sum(const int &a, const int &b);
+        int mult(const int &a, const int &b);
+        int div(const int &a, const int &b);
+        int sub(const int &a, const int &b);
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "lib/math/operations.hpp"
+
 #ifndef CMAKEMACROSEXAMPLE
 #define CMAKEMACROSEXAMPLE "Nothing"
 #endif
@@ -7,4 +9,11 @@
 int main() {
     std::cout << "Hello World" << std::endl;
     std::cout << CMAKEMACROSEXAMPLE << std::endl;
+
+    math::operations op;
+    int sum = op.sum(4, 5);
+
+    std::cout << "Result = " << sum << std::endl;
+
+    return 0;
 }
